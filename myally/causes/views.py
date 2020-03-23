065d632ffaef60654cad505364ab6097b07ada2f
Piotr Zalewa <zaloon@gmail.com>
2020-03-23 23:11:07 +0100

    404

--- myally/causes/views.py
+++ myally/causes/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Cause
 from therapists.models import Therapist
 
 
 def index(request, cause_name):
-    cause = Cause.objects.get(name=cause_name)
+    cause = get_object_or_404(Cause, name=cause_name)
     therapists = cause.therapists.filter(online=True)
     return render(
         request, "therapists.html", context=dict(cause=cause, therapists=therapists)
