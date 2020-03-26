978559ce6757e6a74031a26c5d341242349b5feb
Piotr Zalewa <zaloon@gmail.com>
2020-03-26 15:40:01 +0100

    table instead of ul

--- myally/causes/static/causes/js/therapists.js
+++ myally/causes/static/causes/js/therapists.js
@@ -2,4 +2,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     $('#reload_button').click(function() {
         location.reload();
     });
+    $('[data-toggle="tooltip"]').tooltip()
+    $(".therapists .disabled .a").click(function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
 })
