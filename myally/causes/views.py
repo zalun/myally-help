700db7561be37ad6d3c377bf0b0a9cc349170c2b
Piotr Zalewa <zaloon@gmail.com>
2020-03-26 19:39:29 +0100

    count only active therapists

--- myally/causes/views.py
+++ myally/causes/views.py
@@ -7,10 +7,10 @@ from therapists.models import Therapist
 
 def index(request, cause_name):
     cause = get_object_or_404(Cause, name=cause_name)
-    therapists = cause.therapists.filter(online=True)
+    therapists = cause.therapists.filter(active=True, online=True)
     count_online = therapists.count()
     count_available = therapists.filter(busy=False).count()
-    count_all = cause.therapists.count()
+    count_all = cause.therapists.filter(active=True).count()
     return render(
         request, 
         "therapists.html", 
