22139b4b257d5dd6f1ffcca04cb33a5b46a85e4d
Piotr Zalewa <zaloon@gmail.com>
2020-03-26 11:26:11 +0100

    change content

--- myally/causes/urls.py
+++ myally/causes/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="cause", kwargs=dict(cause_name="coronavirus")),
+    path("", views.index, name="cause", kwargs=dict(cause_name="COVID-19")),
     path("<str:cause_name>", views.index, name="cause"),
 ]
