61ba9062b9110833368e134173202765c15316a1
Piotr Zalewa <zaloon@gmail.com>
2020-03-31 22:14:43 +0200

    --amend

--- myally/therapists/forms.py
+++ myally/therapists/forms.py
@@ -19,8 +19,6 @@ class ShortActivityForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_method = "post"
-        self.helper.add_input(Submit("submit", "Zapisz"))
 
 
 class ShortTherapistForm(forms.Form):
