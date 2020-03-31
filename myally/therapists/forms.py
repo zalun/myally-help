c0c4ca9a6aa319fa794ba18d6a10fa800eabf48d
Piotr Zalewa <zaloon@gmail.com>
2020-03-31 22:12:14 +0200

    no custom switch for all

--- myally/therapists/forms.py
+++ myally/therapists/forms.py
@@ -36,7 +36,6 @@ class ShortTherapistForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.label_class = "custom-switch"
 
         self.helper.form_method = "post"
         # self.helper.add_input(Submit("submit", "Zapisz"))
